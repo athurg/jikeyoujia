8565aa4e204b381c1d1c0b6ba8f098c37b0bd3ba
Athurg Feng <athurg@gooth.org>
2017-08-30 09:17:37 +0800

    增加微信通知的功能

--- signer/main.go
+++ signer/main.go
@@ -5,17 +5,20 @@ import (
 	".."
 
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Fatalf("用法: %s 用户名 密码\n", os.Args[0])
+	if len(os.Args) < 4 {
+		log.Fatalf("用法: %s 用户名 密码 微信通知用户\n", os.Args[0])
 		return
 	}
 
 	username := os.Args[1]
 	password := os.Args[2]
+	toUser := os.Args[3]
 
 	client := jikeyoujia.New()
 	client.EnableDebug()
@@ -23,35 +26,51 @@ func main() {
 	log.Println("登陆")
 	loginInfo, err := client.Login(username, password)
 	if err != nil {
+		Notify(toUser, "吉客优家登陆失败: "+err.Error())
 		log.Fatalf("%s\n", err)
-		return
 	}
+
 	log.Printf("登陆信息: %+v", loginInfo)
 	log.Println("签到前分数:", loginInfo.Score)
 
 	log.Println("获取签到清单")
 	scoreList, err := client.UserScoreList(username)
 	if err != nil {
+		Notify(toUser, "吉客优家获取签到单失败: "+err.Error())
 		log.Fatalf("%s\n", err)
-		return
 	}
 	log.Printf("签到清单信息: %+v", scoreList)
 
 	log.Println("签到")
 	signInfo, err := client.UserSign(username)
 	if err != nil {
+		Notify(toUser, "吉客优家签到失败: "+err.Error())
 		log.Fatalf("%s\n", err)
-		return
 	}
+
 	log.Printf("签到信息: %+v", signInfo)
 
 	log.Println("获取签到后分数")
 	detailInfo, err := client.UserDetail(username)
 	if err != nil {
+		Notify(toUser, "吉客优家获取签到后分数失败: "+err.Error())
 		log.Fatalf("%s\n", err)
-		return
 	}
 
 	log.Printf("签到详情信息: %+v", detailInfo)
 	log.Println("签到后分数:", detailInfo.Score)
+
+	Notify(toUser, "吉客优家签到成功, 分数变化"+loginInfo.Score+" => "+detailInfo.Score)
+}
+
+func Notify(toUser, message string) {
+	log.Println("发送通知", message)
+	getParams := url.Values{}
+	getParams.Set("touser", toUser)
+	getParams.Set("content", message)
+
+	_, err := http.Get("http://localhost:8083/default_notify?" + getParams.Encode())
+	if err != nil {
+		log.Println("微信通知失败", err)
+	}
 }
